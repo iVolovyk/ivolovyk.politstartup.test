0e3903459e93f05af32285339e4aea11686bac5e
Ihor <ihor.volovyk@gmail.com>
2015-07-06 16:53:21 +0300

    ОК

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $phone = trim($_POST["phone"]);
 $email = trim($_POST["email"]);
 $social = trim($_POST["social"]);
 $region = trim($_POST["region"]);
-$message = "Имя: $name \nТелефон: $phone \nПошта: $email \nСоцмережа: $social \nРегіон: $region";
+$message = "ПІБ: $name \nТелефон: $phone \nПошта: $email \nСоцмережа: $social \nРегіон: $region";
 
 $pagetitle = "Нова заявка з сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
