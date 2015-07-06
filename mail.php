adb59753299ff5bc7606217a98ff935535be3e81
Ihor <ihor.volovyk@gmail.com>
2015-07-06 16:19:54 +0300

    initial commit

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "ihor.volovyk@gmail.com";
+$sitename = "Вибори 2015 politstartup.org.ua";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$social = trim($_POST["social"]);
+$region = trim($_POST["region"]);
+$message = "Имя: $name \nТелефон: $phone \nПошта: $email \nСоцмережа: $social \nРегіон: $region";
+
+$pagetitle = "Нова заявка з сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
